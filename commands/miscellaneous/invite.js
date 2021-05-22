64d913df2cc8398eaf859412cc3eecae97550ef4
eDDybruhh <59238354+eDDybruhh@users.noreply.github.com>
2021-05-22 17:56:16 +0300

    Update invite.js

--- commands/miscellaneous/invite.js
+++ commands/miscellaneous/invite.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) =>{
     if(!args[0]) {
         let Sembed = new discord.MessageEmbed()
         .setColor("#5261f8")
-        .addField("***Add Me:***", "[Click to add me to your server!](https://discord.com/oauth2/authorize?client_id=835526427662483526&scope=bot&permissions=268443678)")
+        .addField("***Add Me:***", "[Click to add me to your server!](https://discord.com/oauth2/authorize?client_id=835526427662483526&scope=bot&permissions=470019319)")
         message.channel.send(Sembed)
     }
     

4d23b8804d1b73ef1aadd0887d41261743391e56
eDDybruhh <59238354+eDDybruhh@users.noreply.github.com>
2021-06-04 18:20:08 +0300

    Update shutdown.js

--- commands/owner/shutdown.js
+++ commands/owner/shutdown.js
@@ -3,14 +3,14 @@ module.exports = {
     config: {
         name: "shutdown",
         description: "shuts down the bot!",
-        usage: "k!shutdown",
+        usage: "-shutdown",
         category: "moderation",
         accessableby: "Bot Owner",
         aliases: ["botstop"]
     },
     run: async (bot, message, args) => {
         
-        if(message.author.id != "833635449829457920") return message.channel.send("You're not the bot owner!")
+        if(message.author.id === "377509867923046404" || message.author.id === "788821492611416084" || message.author.id === "282969837490405376"){ 
     
 
     try {
@@ -20,7 +20,8 @@ module.exports = {
         message.channel.send(`ERROR: ${e.message}`)
     }
     
-
+} else {
+    return message.reply("You are not the bot owner!").then(msg => msg.delete(5000))
 
     }
-}
+}}

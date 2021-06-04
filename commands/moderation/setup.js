983601b64f1c1637272f84d704acb9b110f4cfd8
eDDybruhh <59238354+eDDybruhh@users.noreply.github.com>
2021-06-04 18:24:08 +0300

    Update setup.js

--- commands/moderation/setup.js
+++ commands/moderation/setup.js
@@ -27,46 +27,42 @@ module.exports = {
         });
   }
   
-   
- let msg;
-  let embed = new MessageEmbed()
-  .setColor("#5261f8")
-  .setDescription(`***25% Done***`)
-  
-  
-  msg = await message.channel.send(embed)
+    let msg;
+    let embed = new MessageEmbed()
+    .setColor("#f9f9f6")
+    .setDescription(`***25% Done***`)
+    
+    
+    msg = await message.channel.send(embed)
 
 
-  let sembed = new MessageEmbed()
-  .setColor("#5261f8")
-  .setDescription(`***50% Done***`)
-      setTimeout(() => {
-        // Edit msg 20 seconds later
-     msg.edit(sembed);
-      }, 10000);
-
-      let ssembed = new MessageEmbed()
-      .setColor("#5261f8")
-      .setDescription(`***75% Done***`)
-          setTimeout(() => {
+    let sembed = new MessageEmbed()
+    .setColor("#f9f9f6")
+    .setDescription(`***50% Done***`)
+        setTimeout(() => {
             // Edit msg 20 seconds later
-             msg.edit(ssembed);
-          }, 15000);
+        msg.edit(sembed);
+        }, 10000);
 
-          let seembed = new MessageEmbed()
-          .setColor("#5261f8")
-          .setDescription(`
-***25% Done*** - Setupped audit-logs
-***50% Done*** - Setupped Welcome channel
-***75% Done*** - Setupped Leave channel
-***100% Done*** - Fixed everything
-`)
-              setTimeout(() => {
+        let ssembed = new MessageEmbed()
+        .setColor("#f9f9f6")
+        .setDescription(`***75% Done***`)
+            setTimeout(() => {
                 // Edit msg 20 seconds later
-                 msg.edit(seembed);
-              }, 20000);
+                msg.edit(ssembed);
+            }, 15000);
+
+            let seembed = new MessageEmbed()
+            .setColor("#f9f9f6")
+            .setDescription(`
+    ***25% Done*** - Setupped audit-logs
+    ***50% Done*** - Setupped Welcome channel
+    ***75% Done*** - Setupped Leave channel
+    ***100% Done*** - Fixed everything
+    `)
+                setTimeout(() => {
+                    // Edit msg 20 seconds later
+                    msg.edit(seembed);
+                }, 20000);
+        }
     }
-}
-        
-    }
-}

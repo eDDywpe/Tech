7e43e12f99ba1743a13243aec97a3a449d9acc84
eDDybruhh <59238354+eDDybruhh@users.noreply.github.com>
2021-06-04 17:08:41 +0300

    Update setbotstatus.js

--- commands/owner/setbotstatus.js
+++ commands/owner/setbotstatus.js
@@ -14,7 +14,7 @@ module.exports = {
         return message.channel.send("You don't have the permissions to use this command!");
     }
 
-  else if(setStatus[1] === 'idle'){
+    else if(setStatus[1] === 'idle'){
         client.user.setStatus('idle')
             .then(message.channel.send("My status has been set to: "+setStatus[1]))
             .catch(console.error);
@@ -32,14 +32,14 @@ module.exports = {
             .catch(console.error);
     }
 
- else if(setStatus[1] === 'dnd'){
+    else if(setStatus[1] === 'dnd'){
         client.user.setStatus('dnd')
-            .then(message.channel.send("My status has been set to: "+ setStatus[1] + "(Do Not Disturb)"))
+            .then(message.channel.send("My status has been set to: "+ setStatus[1] + "(do not disturb)"))
             .catch(console.error);
     }
 
     else{
-            return message.channel.send("I could not set my status please type one of the following status: idle, online, invisible, dnd (do not disturb)");
+        return message.channel.send("I could not set my status please type one of the following status: idle, online, invisible, dnd (do not disturb)");
     }
 
 }}

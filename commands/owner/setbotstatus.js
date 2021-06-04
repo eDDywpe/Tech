b7573b15cf223c3fbc74fdc7edac4c4e6ca82fc7
eDDybruhh <59238354+eDDybruhh@users.noreply.github.com>
2021-06-04 16:30:41 +0300

    Update setbotstatus.js

--- commands/owner/setbotstatus.js
+++ commands/owner/setbotstatus.js
@@ -9,37 +9,39 @@ module.exports = {
     },
     run: async (client, message, args) => {
     const setStatus = message.content.split(' ');
-    if(message.author.id != "377509867923046404") return message.channel.send("You're not the bot owner!")
+    if(message.author.id === "377509867923046404" || message.author.id === "788821492611416084" || message.author.id === "282969837490405376"){
     if(!message.member.hasPermission("ADMINISTRATOR")){
         return message.channel.send("You don't have the permissions to use this command!");
     }
 
-    else if(setStatus[1] === 'idle'){
-        client.user.setStatus('idle')
+    else if(setStatus[1] === 'Idle'){
+        client.user.setStatus('Idle')
             .then(message.channel.send("My status has been set to: "+setStatus[1]))
             .catch(console.error);
     } 
 
-    else if(setStatus[1] === 'online'){
-        client.user.setStatus('online')
+    else if(setStatus[1] === 'Online'){
+        client.user.setStatus('Online')
             .then(message.channel.send("My status has been set to: "+ setStatus[1]))
             .catch(console.error);
     }
 
-    else if(setStatus[1] === 'invisible'){
-        client.user.setStatus('invisible')
+    else if(setStatus[1] === 'Invisible'){
+        client.user.setStatus('Invisible')
             .then(message.channel.send("My status has been set to: "+ setStatus[1]))
             .catch(console.error);
     }
 
-    else if(setStatus[1] === 'dnd'){
-        client.user.setStatus('invisible')
-            .then(message.channel.send("My status has been set to: "+ setStatus[1] + "(do not disturb)"))
+    else if(setStatus[1] === 'DND'){
+        client.user.setStatus('DND')
+            .then(message.channel.send("My status has been set to: "+ setStatus[1] + "(Do Not Disturb)"))
             .catch(console.error);
     }
 
     else{
-        return message.channel.send("I could not set my status please type one of the following status: idle, online, invisible, dnd (do not disturb)");
+        return message.channel.send("I could not set my status please type one of the following status: Idle, Online, Invisible, DND (Do Not Disturb)");
     }
-
+} else {
+    return message.reply("You are not the bot owner!").then(msg => msg.delete(5000))
+    }
 }}

9db7e51658806102b411320e8fe34b177628525b
eDDybruhh <59238354+eDDybruhh@users.noreply.github.com>
2021-07-21 13:30:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -129,4 +129,26 @@ bot.on("guildBanRemove", function (guild, user) {
     
     `)});
 
+bot.on('clickButton', async (button) => {
+    if (button.id === 'hehe') {
+        await button.clicker.fetch()
+        if(!button.clicker.member.roles.cache.has('851676282407616543')) return button.reply.send("You have no permission to use this.", true)
+        const approvedEmbed = new MessageEmbed()
+            .setTitle('**Bug has been fixed!**')
+            
+            .setAuthor(`${button.clicker.user.username}#${button.clicker.user.discriminator}`, button.clicker.user.displayAvatarURL({ dynamic: true}))
+            .setThumbnail(button.message.embeds[0].thumbnail.url)
+            .setColor('#24fc03')
+            .addField("**Author:**", button.message.embeds[0].fields[0].value)
+            .addField("**Guild:**", button.message.embeds[0].fields[1].value)
+            .addField("**Report of this bug:**", button.message.embeds[0].fields[2].value)
+            .addField("**Fixed by**", `✅ ${button.clicker.user}  fixed this bug.`, true)
+            .setTimestamp();
+          
+        button.message.edit('', { embed: approvedEmbed});
+        button.message.edit({ components: [] })  
+    
+    }
+});
+
 bot.login(token);
